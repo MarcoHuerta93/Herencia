7fd689ca1982a111ebf0b01e39937c121f78e22e
Marco Huerta <marco.huerta.sc@pm.me>
2021-08-09 16:24:24 -0500

    Constructor por defecto y parametrizado

--- Herencia/Publicacion.cs
+++ Herencia/Publicacion.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 namespace Herencia
 {
-    public class Publicacion
+    class Publicacion
     {
+        private static int publicacionID;
+
+
+        //Propiedades
+
+        protected int ID { get; set; }
+
+        protected string Titulo { get; set; }
+
+        protected string Autor { get; set; }
+
+        protected bool EsPublico { get; set; }
+
+        //Constructor por defecto
+
         public Publicacion()
         {
+            ID = 0;
+            Titulo = "Mi primera publicación";
+            EsPublico = true;
+            Autor = "Marco Huerta";
         }
+
+
     }
 }
